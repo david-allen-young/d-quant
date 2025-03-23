8e28fe4a0401446c1bfce5b51c824fd68367e0b4
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-03-22 17:00:21 -0700

    move code into the right module

--- src/rhythmizer/rhythmizer.h
+++ src/rhythmizer/rhythmizer.h
@@ -1,5 +1,3 @@
+// rhythmizer.h
 #pragma once
-#include <string>
-
-void run_envelope_csv_example();
-void run_midi_to_envelope_csv(const std::string& midiFilePath, const std::string& outputDir);
+int rhythmizer_dummy();
