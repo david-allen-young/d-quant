0069e033cf0c7b62ac57d577a8b45ffe9d19c43d
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-04-09 16:27:12 -0700

    Update PipelineArgs.h

--- src/core/PipelineArgs.h
+++ src/core/PipelineArgs.h
@@ -3,7 +3,8 @@
 
 struct PipelineArgs
 {
-        std::string input_midi_path;
+        std::string dynamizer_midi_path;
+        std::string rhythmizer_midi_path;
         std::string envelope_csv_dir;
         std::string morph_csv_dir;
         std::string rhythm_deviation_csv;
