00b4d98d4ac15e89d8d2156ed18bd3622ed1293a
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-04-23 16:52:37 -0700

    refactored to use PathRegistry

--- tests/regression/test_data_utils.h
+++ tests/regression/test_data_utils.h
@@ -7,28 +7,43 @@
 #include "PipelineArgsRegistry.h"
 namespace fs = std::filesystem;
 
+#include "core/PathRegistry.h"
+
 inline void ensure_envelopes_exist()
 {
     const auto& args = getPipelineArgs();
-    const std::string inputMidi = args.dynamizer_midi_path;
-    const std::string envelopeDir = args.envelope_csv_dir;
 
-    if (!fs::exists(envelopeDir) || fs::is_empty(envelopeDir))
+    //const std::string inputMidi = args.dynamizer_midi_path;
+    //const std::string envelopeDir = args.envelope_csv_dir;
+
+    const auto inputBase = PathRegistry::getResolvedPath("input_midi_dir");
+    const auto inputDir = (inputBase / "").lexically_normal();
+    const auto inputPath = (inputDir / "Dynamizer_Training_CRE_pp_to_ff_0001.midi").lexically_normal();
+
+    const auto testsBase = PathRegistry::getResolvedPath("working_dir_tests");
+    const auto envelopesDir = (testsBase / "midi_to_csv").lexically_normal();
+
+    if (!fs::exists(inputDir) || fs::is_empty(inputDir))
     {
-        std::cout << "[SETUP] Generating envelopes from MIDI: " << fs::absolute(inputMidi) << "\n";
-        fs::create_directories(envelopeDir);
-        run_midi_to_envelope_csv(inputMidi, envelopeDir);
+        std::cout << "[SETUP] Generating envelopes from MIDI: " << inputPath << "\n";
+        fs::create_directories(envelopesDir);
+        run_midi_to_envelope_csv(inputPath.string(), envelopesDir.string());
     }
 }
 
 inline void ensure_morphs_exist()
 {
     ensure_envelopes_exist(); // morphs depend on envelope input
-    const auto& args = getPipelineArgs();
-    const std::string morphDir = args.morph_csv_dir;
-    if (!fs::exists(morphDir) || fs::is_empty(morphDir))
+    
+    //const auto& args = getPipelineArgs();
+    //const std::string morphDir = args.morph_csv_dir;
+
+    const auto testsBase = PathRegistry::getResolvedPath("working_dir_tests");
+    const auto morphsDir = (testsBase / "morphs_csv").lexically_normal();
+
+    if (!fs::exists(morphsDir) || fs::is_empty(morphsDir))
     {
         std::cout << "[SETUP] Generating morphs from envelopes...\n";
-        run_morph2_generation_example("test_output/envelopes", morphDir, 10);
+        run_morph2_generation_example("test_output/envelopes", morphsDir.string(), 10);
     }
 }
