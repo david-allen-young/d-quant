dfb5e1b8373436ac73967eebd7f5c74951c7a6b0
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-05-08 15:40:18 -0700

    Create MidiPhraseBuilder.h

---
+++ src/phrase_cli/MidiPhraseBuilder.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "MidiEvent.h"
+#include "NoteInterface.h"
+#include "MidiFileWriter.h"
+#include <string>
+#include <vector>
+
+class MidiPhraseBuilder {
+public:
+    void addNote(const NoteInterface& note, int tpqn, MidiController cc);
+    void writeToFile(const std::string& outPath, int tpqn);
+
+private:
+    std::vector<MidiEvent> events;
+};
