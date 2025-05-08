2478ee00e0b37d7c52b4cd91eef92c26c630d971
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-05-08 15:17:06 -0700

    remove the Point overload

--- src/phrase_cli/CsvWriters.h
+++ src/phrase_cli/CsvWriters.h
@@ -4,6 +4,5 @@
 #include <string>
 #include <utility>
 
-void writeEnvelopeCsv(const std::string& outPath, const std::vector<Point>& ccPoints);
 void writeEnvelopeCsv(const std::string& outPath, const std::vector<std::pair<double, double>>& ccPoints);
 void writeRhythmizerCsv(const std::string& outPath, const std::vector<std::tuple<double, double, double>>& deltas);
