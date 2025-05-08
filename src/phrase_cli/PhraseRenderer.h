142c6b1a8f2229fb62bb70d33f0d1029a8ae07d3
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-05-08 13:40:19 -0700

    Create PhraseRenderer.h

---
+++ src/phrase_cli/PhraseRenderer.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "PhraseArgs.h"
+#include "SongContext.h"
+#include "RenderOptions.h"
+
+void generate_phrase_midi(const PhraseArgs& phrase, const SongContext& context, const RenderOptions& options);
